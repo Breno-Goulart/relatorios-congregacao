a2463ef871520ab04b86b17a81286eb9057192b1
Breno <breno_goulart98@outlook.com>
2026-03-28 20:11:57 -0300

    COMEÇOU

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      workbox: { globPatterns: ['**/*.{js,css,html,ico,png,svg}'] }
-    })
+    react()
   ],
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('/node_modules/react/') || id.includes('/node_modules/react-dom/') || id.includes('/node_modules/react-router-dom/')) {
+          if (
+            id.includes('/node_modules/react/') ||
+            id.includes('/node_modules/react-dom/') ||
+            id.includes('/node_modules/react-router-dom/')
+          ) {
             return 'vendor-react';
           }
           if (id.includes('/node_modules/firebase/')) {
