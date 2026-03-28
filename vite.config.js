ec86505986a8c1ac5d4de8c3e1efbaa364beb3eb
Breno <breno_goulart98@outlook.com>
2026-03-28 17:29:15 -0300

    COMEÇOU

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: { globPatterns: ['**/*.{js,css,html,ico,png,svg}'] }
+    })
+  ],
   build: {
     rollupOptions: {
       output: {
